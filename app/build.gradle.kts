6b651f4a83d388f029ac6900a49441f828154c6d
Ilya Sukhanov <ilya.sukhanovv@gmail.com>
2026-03-08 07:34:49 -0600

    Worked on Firebase implementation & UserId Identifying

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-firestore")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
