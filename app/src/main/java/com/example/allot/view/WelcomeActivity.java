c5b0b63faaf6f690cef38a40f4eb5f47875b134e
Jordan <jordanantonio727@gmail.com>
2026-03-11 23:24:57 -0600

    feat: implemented name screen

--- app/src/main/java/com/example/allot/view/WelcomeActivity.java
+++ app/src/main/java/com/example/allot/view/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.allot.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,10 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button getStartedButton = findViewById(R.id.getStartedButton);
+        getStartedButton.setOnClickListener(view -> {
+            startActivity(new Intent(WelcomeActivity.this, NameActivity.class));
+        });
     }
 }
