a747edf4a9af90a7a6cba2d2e6c9adc310478e21
Jordan <jordanantonio727@gmail.com>
2026-03-12 17:18:00 -0600

    feat: implement phone number

--- app/src/main/java/com/example/allot/view/EmailActivity.java
+++ app/src/main/java/com/example/allot/view/EmailActivity.java
@@ -1,6 +1,8 @@
 package com.example.allot.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,9 @@ public class EmailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
+
+        Button emailNextButton = findViewById(R.id.emailNextButton);
+        emailNextButton.setOnClickListener(view ->
+                startActivity(new Intent(EmailActivity.this, PhoneActivity.class)));
     }
 }
